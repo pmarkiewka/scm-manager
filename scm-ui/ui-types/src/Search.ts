104704f2d48d814d0bda1fdde19f81e5b4a94c16
Konstantin Schaper <konstantin.schaper@cloudogu.com>
2021-08-04 15:59:35 +0200

    fix findings

--- scm-ui/ui-types/src/Search.ts
+++ scm-ui/ui-types/src/Search.ts
@@ -53,10 +53,10 @@ export type QueryResult = PagedCollection<HitEmbedded> & {
 export type SearchableField = {
   name: string;
   type: string;
-}
+};
 
 export type SearchableType = HalRepresentation & {
   name: string;
   type: string;
   fields: SearchableField[];
-}
+};
